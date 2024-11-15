67b55c95fdb0a83a52ed5f1e37d9486ec19ef563
crab.huang <crab.huang@huolala.cn>
2024-11-15 10:39:12 +0800

    feat: first init

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// unocss
+import UnoCSS from 'unocss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS(),
+    basicSsl()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    open: true
+  }
+})
